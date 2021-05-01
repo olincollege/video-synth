6c45e3658479ec04f6a0d9d1324509da02667e59
Max Stopyra <mstopyra@olin.edu>
2021-05-01 10:46:11 -0400

    Files for controller and model

---
+++ controller-video-synth.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class SongController:
+    """
+    """
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def load(self):
+        pass
+
+class YtController(SongController):
+    """
+    """
+    def load(self):
+        pass
+
+class Mp3Controller(SongController):
+    """
+    """
+    def load(self):
+        pass
