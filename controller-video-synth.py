0182210a0165cf3e80835561d4bb86a6456d9adc
Max Stopyra <mstopyra@olin.edu>
2021-05-01 11:26:59 -0400

    Pushing controller for second round of testing

--- controller-video-synth.py
+++ controller-video-synth.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import sys
+from youtube_dl import YoutubeDL
 
 class SongController:
     """
@@ -7,17 +9,31 @@ class SongController:
         pass
 
     @abstractmethod
-    def load(self):
+    def create_file_path(self):
         pass
 
 class YtController(SongController):
     """
     """
-    def load(self):
-        pass
+    def create_file_path(self):
+        file_path = 'audio_files/%(title)s.%(ext)s'
+        yt_url = input('Paste in a youtube link that you want to visualize!')
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': file_path,
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([yt_url])
 
 class Mp3Controller(SongController):
     """
     """
-    def load(self):
+    def create_file_path(self):
         pass
