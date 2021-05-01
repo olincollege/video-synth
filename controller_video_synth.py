829b34fd0d92bf505a2021eedac4d554c63a729d
Rohith Tatineni <Rohith.Tatineni@gmail.com>
2021-05-01 13:44:31 -0400

    Integrated the video synth model and controller

---
+++ controller_video_synth.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+import sys
+from youtube_dl import YoutubeDL
+
+class SongController:
+    """
+    """
+    def __init__(self,VisualSynth):
+        self._visual_synth = VisualSynth
+    
+    @property
+    def visual_synth(self):
+        return self._visual_synth
+    @abstractmethod
+    def create_file_path(self):
+        pass
+
+class YtController(SongController):
+    """
+    """
+    def create_file_path(self):
+        file_path = 'audio_files/%(title)s.%(ext)s'
+        yt_url = input('Paste in a youtube link that you want to visualize!')
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': file_path,
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([yt_url])
+        self.visual_synth.set_filepath(file_path)
+class Mp3Controller(SongController):
+    """
+    """
+    def create_file_path(self):
+        file_path = input('Paste in the file path of the mp3 you want to visualize!')
+        self.visual_synth.set_filepath(file_path)
+    
