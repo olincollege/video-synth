445fa7fe4c89a5cce93d96eeb2b4bba367305411
Max Stopyra <mstopyra@olin.edu>
2021-05-01 20:09:55 -0400

    Integrated librosa to our model.

--- model_video_synth.py
+++ model_video_synth.py
@@ -1,3 +1,7 @@
+import librosa
+import numpy as np
+import librosa.display
+
 class VideoSynth:
     """
     VideoSynth with basic manipulation functionality.
@@ -5,6 +9,9 @@ class VideoSynth:
     Attributes:
         _file_path
         _audio_features_list
+        _harmonic
+        _onset_beat_strength
+        _tempo
     """
     
     def __init__(self):
@@ -22,6 +29,21 @@ class VideoSynth:
     
     def create_audio_features_list(self):
         #LibRosa Stuff
+
+        y, sr = librosa.load(self._file_path)
+        y_harmonic, y_percussive = librosa.effects.hpss(y)
+        chroma_harmonic = librosa.feature.chroma_cqt(y=y_harmonic, sr=sr, bins_per_octave=36)
+        onset_env = librosa.onset.onset_strength(y, sr=sr, aggregate=np.median)
+        tempo = librosa.beat.beat_track(y=y_percussive, sr=sr, onset_envelope=onset_env)
+
+        self._harmonic = chroma_harmonic
+        self._onset_beat_strength = librosa.util.normalize(onset_env)
+        self._tempo = tempo
+        print(f'{len(self._harmonic)} and {len(self._onset_beat_strength)}')
+
+        #self._audio_features_list = LibRosa Data File
+
+    @property
+    def tempo(self):
+        return self._tempo
         
-        #self._audio_features_list == LibRosa Data File
-        pass
